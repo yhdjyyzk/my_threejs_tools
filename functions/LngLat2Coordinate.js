caffdd3ad1aa37b06e9cc90b4b41fd68e2c239f0
yuanzhaokang <kangzhaoyuan@gmail.com>
2018-03-21 10:15:20 +0800

    fix lngLat2Coordinate.

--- functions/LngLat2Coordinate.js
+++ functions/LngLat2Coordinate.js
@@ -2,11 +2,9 @@
  * Created by ZhaokangYuan on 2016/1/15.
  */
 function lngLat2Coordinate(lng, lat, radius) {
-	let len = radius * Math.cos(lat / 180 * Math.PI);
-	let x = radius * Math.sin(lng / 180 * Math.PI);
-	let y = len;
-
-	return {x, y};
+    let x = radius * Math.cos(lng / 180 * Math.PI);
+    let y = radius * Math.tan(lat / 180 * Math.PI);
+    return {x, y};
 }
 
 export default lngLat2Coordinate;
---
+++ test/DrawGeo.js
@@ -0,0 +1,61 @@
+import Polygon from 'zrender/src/graphic/shape/Polygon';
+import Rect from 'zrender/src/graphic/shape/Rect';
+import Group from 'zrender/src/container/Group';
+import LngLat2Coordinate from '../functions/LngLat2Coordinate';
+
+/*
+ * @param {ZRender} zr
+ * @param {Object} geojson. xiping.json
+ **/
+export default function drawGeo(zr, geojson) {
+   let g = new Group();
+   zr.add(g);
+   let center = [400, 400];
+
+   let {features} = geojson;
+   let {geometry} = features[0];
+   let {coordinates} = geometry;
+   let coords = [];
+   let r = 400;
+
+   for(let i = 0; i < coordinates.length; i++) {
+      let coord = coordinates[i];
+
+      for(let j = 0; j < coord.length; j++) {
+         let c = LngLat2Coordinate(coord[j][0], coord[j][1], r);
+         coords.push([400 - c.x, 600 - c.y]);
+      }
+   }
+
+   let p = new Polygon({
+      shape: {
+         points: coords
+      },
+      style: {
+         stroke: 'red',
+         lineWidth: 1,
+         fill: 'blue',
+         strokeNoScale: true
+      },
+      scale: [100, 100],
+   });
+
+   let bounding = p.getBoundingRect();
+   let origin = [
+      bounding.x + bounding.width / 2,
+      bounding.y + bounding.height / 2
+   ];
+   p.attrKV("origin", origin);
+
+   let rect = new Rect({
+      shape: p.getBoundingRect(),
+      style:{
+         stroke: 'red',
+         lineWidth: 2,
+         fill: null
+      }
+   });
+
+   g.add(p);
+   g.add(rect);
+}
